56636f96e44962474b296cf3b72d623623a4bed2
Anton Bessonov <gh@bessonov.de>
2015-05-01 14:55:25 +0200

    fix(tagsInput): ie8 doesn't support setAttribute for type

--- src/bind-attrs.js
+++ src/bind-attrs.js
@@ -8,12 +8,18 @@
  * @description
  * Binds attributes to expressions. Used internally by tagsInput directive.
  */
-tagsInput.directive('tiBindAttrs', function() {
+tagsInput.directive('tiBindAttrs', ['$sniffer', function($sniffer) {
+    var typeUnsupported = $sniffer.msie && $sniffer.msie <= 8;
     return function(scope, element, attrs) {
         scope.$watch(attrs.tiBindAttrs, function(value) {
             angular.forEach(value, function(value, key) {
-                attrs.$set(key, value);
+                // ie8 doesn't support setAttribute for type, see
+                // https://msdn.microsoft.com/en-us/library/ms534700.aspx
+                // so let fallback to "text"
+                if (!(key === 'type' && typeUnsupported)) {
+                    attrs.$set(key, value);
+                }
             });
         }, true);
     };
-});
+}]);
